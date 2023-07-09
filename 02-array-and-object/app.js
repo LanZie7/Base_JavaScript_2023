9abefd284c04df4c11ce2fb4f4e71c177366ba7e
bochkova_su <bochkova_su@resanta.ru>
2023-07-09 21:31:18 +0300

    Массивы и объекты

--- 02-array-and-object/app.js
+++ 02-array-and-object/app.js
@@ -142,13 +142,11 @@ function getNoteTemplate(note, index) {
     <li
       class="list-group-item d-flex justify-content-between align-items-center"
     >
-      <span class="${note.completed ? 'text-decoration-line-through' : ''}">${
-    note.title
-  }</span>
+      <span class="${note.completed ? 'text-decoration-line-through' : ''}">
+        ${note.title}
+      </span>
       <span>
-        <span class="btn btn-small btn-${
-          note.completed ? 'warning' : 'success'
-        }" data-index="${index}" data-type="toggle">&check;</span>
+        <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
         <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
       </span>
     </li>
